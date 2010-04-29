ff7baf044e555204d141ca7503da50b04a5e50b6
Michael Kuhn <suraia@ikkoku.de>
2010-04-29 22:54:01 +0200

    Update release tool.

--- tools/version.sh
+++ tools/version.sh
@@ -27,56 +27,27 @@
 
 set -e
 
+bump_version ()
+{
+	file="$1"
+	version="$2"
+
+	sed -i "/^VERSION = /s/^.*$/VERSION = '${version}'/" "${file}"
+
+	git add "${file}"
+	git commit -e -m "Bump version to ${version}."
+}
+
 usage ()
 {
-	echo "Usage: ${0##*/} [branch] release"
+	echo "Usage: ${0##*/} file version"
 	exit 1
 }
 
 test -z "$1" && usage
+test -z "$2" && usage
 
-if [ ! -f ./tools/release.sh ]
-then
-	echo 'Not in top-level directory.'
-	exit 1
-fi
+file="$1"
+version="$2"
 
-if [ $# -lt 2 ]
-then
-	branch='master'
-	release="$1"
-else
-	branch="$1"
-	release="$2"
-fi
-
-sushi_release="sushi-${release}"
-
-rm -fv "${sushi_release}.tar" "${sushi_release}.tar.bz2" "${sushi_release}.tar.gz"
-
-git checkout "${branch}"
-git tag "${release}"
-git archive --prefix="${sushi_release}/" "${release}" > "${sushi_release}.tar"
-
-for component in maki tekka nigiri plugins
-do
-	component_release="${component}-${release}"
-
-	cd "../${component}"
-
-	git checkout "${branch}"
-	git tag "${release}"
-	git archive --prefix="${sushi_release}/${component}/" "${release}" > "../suite/${component_release}.tar"
-
-	git checkout master
-
-	cd ../suite
-
-	tar Avf "${sushi_release}.tar" "${component_release}.tar"
-	rm -fv "${component_release}.tar"
-done
-
-git checkout master
-
-bzip2 -k "${sushi_release}.tar"
-gzip "${sushi_release}.tar"
+bump_version "${file}" "${version}"
